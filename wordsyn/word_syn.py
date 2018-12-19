050f96fb554f31ec3d6317036ec64793276aaf8c
kennethli319 <liwangyau@gmail.com>
2018-12-19 10:32:15 +0000

    Update word_syn.py

--- wordsyn/word_syn.py
+++ wordsyn/word_syn.py
@@ -11,14 +11,16 @@ Usage:
     python word_syn.py <input_sequence> <language: c or p>
 
 Example:
-    python word_syn.py 翻译都要执行多个翻译系统，这带来巨大的计算成本。如今，许多领域都正在被神经网路技术颠覆。 -l p
-    python word_syn.py 翻譯都要執行多個翻譯系統，這帶來巨大的計算成本。如今，許多領域都正在被神經網路技術顛覆。 -l c
+    python3 word_syn.py 1/1/2001，翻译都要执行多个翻译系统，这带来巨大的计算成本。如今，许多领域都正在被神经网路技术颠覆。 -l p -p -v 80
+    python3 word_syn.py 1/01/1991，翻译都要执行多个翻译系统，这带来巨大的计算成本。如今，许多领域都正在被神经网路技术颠覆。 -l c -p -v 80
 """
 
 # LOGBK and PROBLEMS
 """
-16DEC - Done word-wav data in Can and Manderin
-18DEC - Done overall documentation
+16 DEC - Done word-wav data in Can and Manderin
+18 DEC - Done overall documentation
+19 DEC - (ING) Class structure
+19 DEC - NSW Date conversion
 
 1 - check sim chin / tran chin, if option given = follow option, if not check number of words in sim, if > 50%, mandarin else cantonese
 2 - cantonese corpus = very slow -> change it to a txt file with count / % only 
@@ -31,7 +33,7 @@ Example:
 # Pipeline
 """
 STRUCTURE
-1 - CLASS:  Seq:    sequence
+1 - CLASS:  Sequence:    sequence
             Char:   char info
                     i)      Char
                     ii)     CV - onset, n, coda
@@ -95,7 +97,8 @@ parser.add_argument('--language', "-l", action="store", dest="language", type=st
 parser.add_argument('--play', '-p', action="store_true", default=False, help="Play the output audio")
 parser.add_argument('--outfile', '-o', action="store", dest="outfile", type=str, help="Save the output audio to a file", default=None)
 parser.add_argument('--volume', '-v', default=None, type=int, help="An int between 0 and 100 representing the desired volume")
-# FOLLOWUP: Add -> voice optioins? speed? emotion? 
+parser.add_argument('--speed', '-s', default=None, type=float, help="A float between 0 - 3 representing the desired speed")
+# FOLLOWUP: Add -> voice options? speed? emotion? 
 
 # (1.2) Parse arguments from the command line
 try: 
@@ -143,45 +146,206 @@ def assign_paths(language):
 # (1.3) Select reuired database/dictionary accoring to the given lang option
 path, dictpath = assign_paths(args.language)
 
-# (PART 2) Define Methods and Classes
-# (2.1) Methods
-def strB2Q(ustring):
-    # modified from https://codertw.com/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80/373914/
-    # DOESNT WORK SAD
-    """把字串半形轉全形"""
-    rstring = ""
-    for uchar in ustring:
-        inside_code=ord(uchar)
-    if inside_code<0x0020 or inside_code>0x7e:   #不是半形字元就返回原來的字元
-        rstring  = uchar
-    if inside_code==0x0020: #除了空格其他的全形半形的公式為:半形=全形-0xfee0
-        inside_code=0x3000
-    else:
-        inside_code =0xfee0
-        rstring  = chr(inside_code)
-    return rstring
+# (PART 2) Define Functions and Classes
+"""
+(2.1) Operation functions
 
-def normalizarion(inputString):
-    outputString = strB2Q(inputString)
-    return outputString
 
-def text_conversion(input_sequence):
-    """S2T/T2S Conversion by OpenCC (https://github.com/BYVoid/OpenCC)"""
-    # convert from Traditional Chinese to Simplified Chinese
-    if args.language == 'p':
-        cc = OpenCC('t2s') 
-    # convert from Simplified Chinese to Traditional Chinese
-    elif args.language == 'c':
-        cc = OpenCC('s2t')  
-    return cc.convert(input_sequence)
+(2.2) Classes
+    Sequence()  : Sequence of surface utterance, attributes including surface form, list of token objects
+    Tokens()    : List of single/multiple Char instance, attributes including position marker, 
+    Char()      : Single char
 
-# (2.2) Classes
-class Seq:
+(2.3) User interface functions
+    adjust_volume() : Volume Control
+    save()          : Basic user interface to save the audio
+    play_audio()    : Basic user interface to play the audio
+"""
+
+class Sequence:
     """
     seq info, contain char info in each item in a list
     """
-    def __init__(self, string):
-        self.seqitem = list(string)
+
+    def __init__(self, string=""): 
+        
+        # (Step 0) - Define attributes
+        self.utterance = ""
+        self.norm_utterance = ""
+        self.tokens = []
+        # (Step 1) - Entire pipeline structure for TTS
+        self.sayText(string)
+        # (Step 2) - Print all linguistic infomation
+        self.print_seq_info()
+
+    def sayText(self,string):
+        self.utterance = string
+        self.norm_utterance = self.normalize(self.utterance) 
+        self.tokens = list(self.norm_utterance)
+        
+    def normalize(self, string):
+        string = self.punct_conversion(string)
+        # Convert S2T or T2S to avoid mixed variaty text encoding
+        string = self.text_conversion(string)
+        string = self.nsw_conversion(string)
+        return string
+
+    def punct_conversion(self, string):
+        # FOLLOWUP: Remove unnecessary punct
+        # FOLLOWUP: Convert half punct to full 
+        return string
+
+    def text_conversion(self, string):
+        """S2T/T2S Conversion by OpenCC (https://github.com/BYVoid/OpenCC)"""
+        # convert from Traditional Chinese to Simplified Chinese
+        if args.language == 'p':
+            cc = OpenCC('t2s') 
+        # convert from Simplified Chinese to Traditional Chinese
+        elif args.language == 'c':
+            cc = OpenCC('s2t')  
+        return cc.convert(string)
+
+    def nsw_conversion(self, string):
+        string = self.translate_num_pattern(string)
+        return string
+    
+    # Text Normalization for dates and all number expressions
+    def translate_num_pattern(self, text):
+        '''
+        Description: Search and translate number expressions, such as stand alone numbers or date expression
+        
+        Input : A string that contains sequence of text (with raw numbers)
+        Output: Normalized text (with numbers as words) as a new string
+        '''
+        searching = True
+        number_pattern = r"([^0-9]*)([0-9]+/*[0-9]*/*[0-9]*)([^0-9]*)"
+        # Continuous searching for number expressions (including stand alone numbers and date expressions)
+        while searching == True:
+            # Detect if there is any number expressions in the text
+            if re.match(number_pattern, text):
+                # If yes, extract the number portion (group 2) and convert it to words
+                num = re.match(number_pattern, text)
+                number_word = self.number_to_word(num.group(2))
+                # Replace the numbers with their corresponding pronunciable words
+                text = text.replace(num.group(2),number_word,1)
+            else:
+                # No further match of numbers, then stop searching for number patterns
+                searching = False
+                break
+        return text
+
+    # Text Normalisation for numbers in date expressions
+    def number_to_word(self, number_seq):
+        '''
+        Description: Convert numbers to words
+
+        Input : A string that contains number sequence
+        Output: Translated number in words as a new string
+        '''
+        output =""
+        
+        # For date expressions, split the input by the delimilater "/"
+        number_list = number_seq.split("/")
+
+        # Handle stand alone numbers
+        if len(number_list) == 1:
+            num = number_list[0]
+            output = self.construct_number_words(num)
+
+        # Handle date and month coversion, and optional year conversion
+        elif len(number_list) >= 2:
+            # Get words for date and month from a full entry dictionary
+            date = int(number_list[0])
+            assert date <= 31, "ERROR: Incorrect date format (date > 31 in DD/MM/YY)"
+            assert date > 0, "ERROR: Incorrect date format (date < 0 in DD/MM/YY)"
+            month = int(number_list[1])
+            assert month <= 12, "ERROR: Incorrect month format (month > 12 in DD/MM/YY)"
+            assert month > 0, "ERROR: Incorrect month format (month < 0 in DD/MM/YY)" 
+            # Combine the converted words to the output
+            output = self.construct_number_words(month) + "月" + self.construct_number_words(date) + "日"
+
+            # Coversion of year part
+            if len(number_list) == 3:
+                year = number_list[2]
+                # NOTE: Assume this program in default will deal with years in 20xx
+                year_output = "二零"    
+                # (EXTRA) Handle years outside the range of 20xx
+                for each in year:
+                    char = self.construct_number_words(each)
+                    year_output += char
+                if len(year) == 4:
+                    year_output = ""
+                    for each in year:
+                        char = self.construct_number_words(each)
+                        year_output += char
+                output = year_output + "年" + output 
+
+        # Provide a message to inform users about the auto number/date conversion
+        print("Translated number expressions: " + number_seq + " ->" + re.sub("\s+"," ",output))
+
+        return output
+
+    # Extension F - Text Normalisation for number expressions
+    # (EXTRA) Translates stand alone number expressions (range: 0-99)
+    # (EXTRA) For long number, spell out digits one by one
+    def construct_number_words(self, number):
+        '''
+        Description: Convert numbers to words
+
+        Input : A string that contains number sequence
+        Output: Translated number in words as a new string
+        '''
+
+        # Words corresponding to the numbers in digits
+        dict_digit = {"0": "零", "1" : "一", "2" : "二", "3" : "三", "4" : "四", "5" : "五", "6" : "六", "7" : "七", "8" : "八", "9" : "九", "10" : "十"}
+        
+        number = str(number)
+        
+        # Contruct number to words
+        try:
+            # Pick their corresponding words in the dictionary 
+            number_words = dict_digit[number]
+        except:
+            # If the expression is not in the dictionary, decompose the number to the ten position and digit position,
+            # then recreate it based on the numerical value
+            tens = str(int(number)//10)
+            remainder = str(int(number) % 10)
+            # Look for their word forms seperatly
+            number_words = dict_digit[tens] + "十" + dict_digit[remainder]
+        # Return the word form of number expressions
+        return number_words
+
+    # FOLLOWUP: SUPER SLOW!
+    def word_seg(self, string):
+        """Word seg"""
+        seg = pkuseg.pkuseg()   #以默认配置加载模型
+        tokens = seg.cut(string)	#进行分词
+        return tokens
+
+    def print_seq_info(self):
+        pprint("Surface utterance sequence: {}".format(self.utterance))
+        pprint("Normalized utterance sequence: {}".format(self.norm_utterance))
+        pprint("List of tokens: {}".format(self.tokens))
+        
+    # def strB2Q(ustring):
+    #     # modified from https://codertw.com/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80/373914/
+    #     # DOESNT WORK SAD
+    #     """把字串半形轉全形"""
+    #     rstring = ""
+    #     for uchar in ustring:
+    #         inside_code=ord(uchar)
+    #     if inside_code<0x0020 or inside_code>0x7e:   #不是半形字元就返回原來的字元
+    #         rstring  = uchar
+    #     if inside_code==0x0020: #除了空格其他的全形半形的公式為:半形=全形-0xfee0
+    #         inside_code=0x3000
+    #     else:
+    #         inside_code =0xfee0
+    #         rstring  = chr(inside_code)
+    #     return rstring
+
+    # def normalizarion(inputString):
+    #     outputString = strB2Q(inputString)
+    #     return outputString
 
 class Char:
     """
@@ -205,39 +369,62 @@ class Char:
         string = re.sub(r"[：；。？！]", "sil_400", string)
         return string
 
-# Main module
-if __name__ == "__main__":
-        
-    # Convert S2T or T2S to avoid mixed variaty text encoding
+# (2.3) User interface functions
 
-    # input seq get
-    inputseq = args.phrase[0]
-    inputseq = text_conversion(inputseq)
+def adjust_volume(volume=None, object=None):
+    """
+    Description: Volume Control
     
-    inputseq = Seq(inputseq)
+    Input: Required volume adjustment value 0 to 100 and the object to adjust
+    Output:The volume adjusted audio object
+    """
+    if volume != None:
+        # Ensure the volume scaling is in the expected range
+        if volume < 0 or volume > 100:
+            raise ValueError("Expected scaling factor between 0 and 100.")
+        # Conver the input int 0-100 to a float number between 0-1 and rescale accordingly
+        object.rescale(volume/100.0)
+    # Return the modified audio object
+    return object 
 
-    # FOLLOWUP: SUPER SLOW!
-    def word_seg(input_string):
-        """Word seg"""
-        seg = pkuseg.pkuseg()   #以默认配置加载模型
-        text = seg.cut(input_string)	#进行分词
-        return text
+def save(output_file=None, object=None):
+    """
+    Description: Basic user interface to save the audio
+    """
+    if output_file != None:
+        object.save(output_file)
+        print("It is saved as:", output_file)
+        # (EXTRA) Ensure user understand the potential error
+        if ".wav" not in output_file:
+            print("*** WARNING: File might not be saved properly if your file extension is not .wav")
 
-    # inputseq = Seq(normalizarion(inputseq))
+def play_audio(play=False, object=None):
+    """
+    Description: Basic user interface to play the audio
+    """
+    if play == True:
+        object.play()
+
+# Main module
+def main():
+    # Step 1 - Get input utterance sequence
+    inputseq = args.phrase[0]
+    # Step 2 - Put the text in a Sequence instance
+    inputseq = Sequence(inputseq)
 
     # hkcan_corpus = pc.hkcancor()
-    # for each in inputseq.seqitem:
+    # for each in inputseq.tokens:
     #     wordinfo = hkcan_corpus.search(character=each)
         # pprint(len(wordinfo))
         # pprint(wordinfo[:3])
 
     # Turn to class instance
     index_of_item = 0
-    for each in inputseq.seqitem:
-        inputseq.seqitem[index_of_item] = Char(each)
+    for each in inputseq.tokens:
+        inputseq.tokens[index_of_item] = Char(each)
         index_of_item += 1
 
-    for each in inputseq.seqitem:
+    for each in inputseq.tokens:
 
         each.eachphone = simpleaudio.Audio()
 
@@ -259,7 +446,18 @@ if __name__ == "__main__":
 
     output = simpleaudio.Audio()
 
-    for each in inputseq.seqitem:
+    for each in inputseq.tokens:
         output.data = np.concatenate((output.data, each.eachphone.data))
 
-    output.play()
+    # Step 5 - Further adjustment on overall volume to the final output (if the user use -v <0-100>)
+    output = adjust_volume(volume=args.volume, object=output)
+
+    # Step 6 - Save it to the target file (if the user use -o <args.outfile>)
+    save(output_file=args.outfile, object=output)
+    
+    # Step 7 - Play the final sound output (if the user use -p)
+    play_audio(play=args.play, object=output)
+
+if __name__ == "__main__":
+    main()
+    
